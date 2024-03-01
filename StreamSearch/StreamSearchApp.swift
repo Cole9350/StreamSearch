eb93b0cef7e3a7726fc4978f591f799fdea1de44
Shawn Cole <scole@shawns-air.mynetworksettings.com>
2024-03-01 15:21:49 -0500

    init repo

---
+++ StreamSearch/StreamSearchApp.swift
@@ -0,0 +1,32 @@
+//
+//  StreamSearchApp.swift
+//  StreamSearch
+//
+//  Created by Shawn Cole on 3/1/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct StreamSearchApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
